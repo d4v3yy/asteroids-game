2d81943ce283837b942480808fd7724c636fd018
d4v3yy <censai14sere@gmail.com>
2025-09-22 18:35:18 -0400

    Fixed bug with game not starting

--- asteroid.py
+++ asteroid.py
@@ -1,13 +1,12 @@
-from circleshape import CircleShape
 import pygame
-from main import screen
+from circleshape import CircleShape
 
 
 class Asteroid(CircleShape):
-    def  __init__(self, x, y, radius):
+    def __init__(self, x, y, radius):
         super().__init__(x, y, radius)
 
-    def draw(self):
+    def draw(self, screen):
         pygame.draw.circle(screen, "white", self.position, self.radius, 2)
 
     def update(self, dt):
--- main.py
+++ main.py
@@ -1,18 +1,16 @@
-#Allows us to use code from the open-source pygame library
 import pygame
-from constants import SCREEN_WIDTH, SCREEN_HEIGHT
+from constants import *
 from player import Player
 from asteroid import Asteroid
 from asteroidfield import AsteroidField
 
 
 def main():
-    #Initatie the game, screen and clock for the game
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
 
-    #Create the groups to declutter the game loop
+    updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
     asteroids = pygame.sprite.Group()
 
@@ -20,7 +18,8 @@ def main():
     AsteroidField.containers = updatable
     asteroid_field = AsteroidField()
 
-    #Instantiate the Player
+    Player.containers = (updatable, drawable)
+
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     dt = 0
@@ -41,6 +40,7 @@ def main():
 
         # limit the framerate to 60 FPS
         dt = clock.tick(60) / 1000
+
 
 if __name__ == "__main__":
     main()
